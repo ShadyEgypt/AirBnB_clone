2c1c2bef55e4f5df1ab73f50a36fc85f30c1f739
Tokaallah <tokallah.92@gmail.com>
2023-07-15 10:28:09 +0000

    [Feature]: amenity model

---
+++ amenity.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""A module to creates Amenity class"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Class of amenity objects"""
+
+    name = ""
